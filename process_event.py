861812c5a68cc07400a8bae75c505d452f01cbf8
Victor Lin <bornstub@gmail.com>
2013-12-04 18:32:17 -0800

    Update script for processing input samples

--- process_event.py
+++ process_event.py
@@ -21,6 +21,14 @@ class EventProcessor(object):
         evs = event_json['entity_views']
         ev10 = evs['1.0']
 
+        total_fee = ev10['total_fee']
+        adjustments_total_fee = ev10['adjustments_total_fee']
+        marketplace_uri = ev10['marketplace_uri']
+        # it is possible a marketplace exists without a bank account associated
+        # with it, in that case, we just create the invoice wihout `payment_uri`
+        # and update it later when the marketplace got a bank account
+        source_uri = ev10.get('source_uri')
+
         hold_item = dict(
             type='Holds',
             quantity=ev10['holds_count'],
@@ -108,11 +116,6 @@ class EventProcessor(object):
                 item['total'],
             )
 
-        total_fee = ev10['total_fee']
-        adjustments_total_fee = ev10['adjustments_total_fee']
-        source_uri = ev10['source_uri']
-        marketplace_uri = ev10['marketplace_uri']
-
         self.logger.info('Total fee: %s', total_fee)
         self.logger.info('Adjustment fee: %s', adjustments_total_fee)
         self.logger.info('Source URI: %s', source_uri)
@@ -132,10 +135,10 @@ class EventProcessor(object):
         #return
 
         api = BillyAPI(
-            api_key='Ehuk9jkcENFxv1Jk2bprUUtrAATv8vNkRw9Fa3m1SkYd', 
+            api_key='FXSpohZE8NuUn5W1peb4FhcXQp49Bc1vDpLkCsPstNRS', 
             endpoint='http://127.0.0.1:6543',
         )
-        company = api.get_company('CPTQhADwa9wJYSspzLDxFEn3')
+        company = api.get_company('CPXVsVvBDY94p3zVNvLSZ21M')
         # TODO: figure how to create a customer from marketplace URI,
         # they should be 1:1 relation in v1.1 API
 
@@ -150,18 +153,12 @@ class EventProcessor(object):
         # TODO: should get existing customer payment method
         # tokenlize a payment method
         balanced.configure('ef13dce2093b11e388de026ba7d31e6f')
-        card = balanced.Card(
-            expiration_month='12',
-            security_code='123',
-            card_number='5105105105105100',
-            expiration_year='2020'
-        ).save()
         # call to billy API, create an invoice
         try:
             invoice = customer.invoice(
                 title='Balanced Transaction Usage Invoice',
                 amount=total_fee,
-                payment_uri=card.uri,
+                payment_uri=source_uri,
                 items=items,
                 adjustments=adjustments,
                 external_id=event_json['guid'],
@@ -177,351 +174,25 @@ class EventProcessor(object):
 
 
 def main():
-    paylod = '''{
-  "trace_guid": "TR7b433aa35d2f11e3b7b428cfe9603dcf",
-  "parent_guid": null,
-  "entity_view": {
-    "bank_account_credits_count": 12,
-    "settlement_uri": "/v1/invoice_settlements/invoice_settlement",
-    "lost_debit_chargebacks_total_fee": 0,
-    "adjustments": [],
-    "updated_at": "2013-11-10T02:01:08.980658Z",
-    "lost_debit_chargebacks_total_amount": 0,
-    "bank_account_debit_variable_fee_cap": 500,
-    "refunds_count": 0,
-    "bank_account_debits_total_amount": 0,
-    "holds_count": 219,
-    "id": "IV4GzXxAvn5WhFBZR9RosL6U",
-    "holds_total_amount": 1378117,
-    "reversals_total_fee": 0,
-    "marketplace_uri": "/v1/marketplaces/MP35BtXWqGuYEsv2RHH2CGtH",
-    "refund_fee": true,
-    "state": "paid",
-    "settle_at": "2013-11-10T01:29:54.315160Z",
-    "card_debits_total_amount": 741118,
-    "holds_total_fee": 0,
-    "reversals_total_amount": 0,
-    "card_debits_count": 171,
-    "debits_total_fee": 24086,
-    "bank_account_credit_fee": 0,
-    "refunds_total_fee": 0,
-    "source_uri": "/v1/marketplaces/MP35BtXWqGuYEsv2RHH2CGtH/bank_accounts/BA3EcoA5u8GOmm1EipSUDlPM",
-    "bank_account_credits_total_fee": 0,
-    "period": [
-      "2013-11-09T00:01:31.148171Z",
-      "2013-11-09T23:57:33.609046Z"
-    ],
-    "failed_credits_count": 0,
-    "refunds_total_amount": 0,
-    "_uris": {
-      "source_uri": {
-        "_type": "bank_account",
-        "key": "source"
-      },
-      "settlement_uri": {
-        "_type": "invoice_settlement",
-        "key": "settlement"
-      }
-    },
-    "adjustments_total_fee": 0,
-    "failed_credit_fee": 0,
-    "reversals_count": 0,
-    "bank_account_debits_total_fee": 0,
-    "bank_account_debits_count": 0,
-    "lost_debit_chargebacks_count": 0,
-    "hold_fee": 0,
-    "bank_account_credits_total_amount": 1166031,
-    "debits_total_amount": 741118,
-    "uri": "/v1/invoices/IV4GzXxAvn5WhFBZR9RosL6U",
-    "card_debits_total_fee": 24086,
-    "bank_account_debit_variable_fee_percentage": 1.0,
-    "chargeback_fixed_fee": 1500,
-    "adjustments_count": 0,
-    "total_fee": 24086,
-    "variable_fee_percentage": 3.25,
-    "failed_credits_total_amount": 0,
-    "debits_count": 171,
-    "failed_credits_total_fee": 0,
-    "_type": "invoice",
-    "created_at": "2013-11-10T01:29:54.367778Z",
-    "sequence_number": 325
-  },
-  "entity_views": {
-    "1.poshmark": {
-      "invoices": [
-        {
-          "created_at": "2013-11-10T01:29:54.367778Z",
-          "bank_account_credits_count": 12,
-          "links": {
-            "source": "BA3EcoA5u8GOmm1EipSUDlPM",
-            "settlement": null
-          },
-          "adjustments": [],
-          "updated_at": "2013-11-10T02:01:08.980658Z",
-          "marketplace_uri": "/marketplaces/MP35BtXWqGuYEsv2RHH2CGtH",
-          "failed_credits_total_fee": 0,
-          "bank_account_debit_variable_fee_cap": 500,
-          "refunds_count": 0,
-          "bank_account_debits_total_amount": 0,
-          "holds_count": 219,
-          "id": "IV4GzXxAvn5WhFBZR9RosL6U",
-          "holds_total_amount": 1378117,
-          "reversals_total_fee": 0,
-          "lost_debit_chargebacks_total_amount": 0,
-          "failed_credit_fee": 0,
-          "state": "paid",
-          "chargeback_fixed_fee": 1500,
-          "reversals_count": 0,
-          "card_debits_total_amount": 741118,
-          "holds_total_fee": 0,
-          "reversals_total_amount": 0,
-          "card_debits_count": 171,
-          "debits_total_fee": 24086,
-          "bank_account_credit_fee": 0,
-          "refunds_total_fee": 0,
-          "bank_account_credits_total_fee": 0,
-          "period": [
-            "2013-11-09T00:01:31.148171Z",
-            "2013-11-09T23:57:33.609046Z"
-          ],
-          "failed_credits_count": 0,
-          "refunds_total_amount": 0,
-          "adjustments_total_fee": 0,
-          "refund_fee": true,
-          "settle_at": "2013-11-10T01:29:54.315160Z",
-          "bank_account_debits_total_fee": 0,
-          "lost_debit_chargebacks_count": 0,
-          "hold_fee": 0,
-          "bank_account_credits_total_amount": 1166031,
-          "debits_total_amount": 741118,
-          "uri": "/invoices/IV4GzXxAvn5WhFBZR9RosL6U",
-          "card_debits_total_fee": 24086,
-          "bank_account_debit_variable_fee_percentage": 1.0,
-          "href": "/invoices/IV4GzXxAvn5WhFBZR9RosL6U",
-          "adjustments_count": 0,
-          "total_fee": 24086,
-          "variable_fee_percentage": 3.25,
-          "failed_credits_total_amount": 0,
-          "debits_count": 171,
-          "lost_debit_chargebacks_total_fee": 0,
-          "bank_account_debits_count": 0,
-          "sequence_number": 325
-        }
-      ],
-      "links": {
-        "invoices.source": "/resources/{invoices.source}",
-        "invoices.settlement": "/invoice_settlements/{invoices.invoice_settlement}"
-      }
-    },
-    "1.0": {
-      "created_at": "2013-11-10T01:29:54.367778Z",
-      "bank_account_credits_count": 12,
-      "settlement_uri": "/v1/invoice_settlements/invoice_settlement",
-      "adjustments": [],
-      "updated_at": "2013-11-10T02:01:08.980658Z",
-      "marketplace_uri": "/v1/marketplaces/MP35BtXWqGuYEsv2RHH2CGtH",
-      "failed_credits_total_fee": 0,
-      "bank_account_debit_variable_fee_cap": 500,
-      "refunds_count": 0,
-      "bank_account_debits_total_amount": 0,
-      "holds_count": 219,
-      "id": "IV4GzXxAvn5WhFBZR9RosL6U",
-      "holds_total_amount": 1378117,
-      "reversals_total_fee": 0,
-      "lost_debit_chargebacks_total_amount": 0,
-      "failed_credit_fee": 0,
-      "state": "paid",
-      "reversals_count": 0,
-      "card_debits_total_amount": 741118,
-      "holds_total_fee": 0,
-      "reversals_total_amount": 0,
-      "card_debits_count": 171,
-      "debits_total_fee": 24086,
-      "bank_account_credit_fee": 0,
-      "refunds_total_fee": 0,
-      "source_uri": "/v1/marketplaces/MP35BtXWqGuYEsv2RHH2CGtH/bank_accounts/BA3EcoA5u8GOmm1EipSUDlPM",
-      "bank_account_credits_total_fee": 0,
-      "period": [
-        "2013-11-09T00:01:31.148171Z",
-        "2013-11-09T23:57:33.609046Z"
-      ],
-      "failed_credits_count": 0,
-      "refunds_total_amount": 0,
-      "_uris": {
-        "source_uri": {
-          "_type": "bank_account",
-          "key": "source"
-        },
-        "settlement_uri": {
-          "_type": "invoice_settlement",
-          "key": "settlement"
-        }
-      },
-      "adjustments_total_fee": 0,
-      "refund_fee": true,
-      "settle_at": "2013-11-10T01:29:54.315160Z",
-      "bank_account_debits_total_fee": 0,
-      "lost_debit_chargebacks_count": 0,
-      "hold_fee": 0,
-      "bank_account_credits_total_amount": 1166031,
-      "debits_total_amount": 741118,
-      "uri": "/v1/invoices/IV4GzXxAvn5WhFBZR9RosL6U",
-      "card_debits_total_fee": 24086,
-      "bank_account_debit_variable_fee_percentage": 1.0,
-      "chargeback_fixed_fee": 1500,
-      "adjustments_count": 0,
-      "total_fee": 24086,
-      "variable_fee_percentage": 3.25,
-      "failed_credits_total_amount": 0,
-      "debits_count": 171,
-      "lost_debit_chargebacks_total_fee": 0,
-      "_type": "invoice",
-      "bank_account_debits_count": 0,
-      "sequence_number": 325
-    },
-    "1.1": {
-      "invoices": [
-        {
-          "created_at": "2013-11-10T01:29:54.367778Z",
-          "bank_account_credits_count": 12,
-          "links": {
-            "source": "BA3EcoA5u8GOmm1EipSUDlPM",
-            "settlement": null
-          },
-          "adjustments": [],
-          "updated_at": "2013-11-10T02:01:08.980658Z",
-          "marketplace_uri": "/marketplaces/MP35BtXWqGuYEsv2RHH2CGtH",
-          "failed_credits_total_fee": 0,
-          "bank_account_debit_variable_fee_cap": 500,
-          "refunds_count": 0,
-          "bank_account_debits_total_amount": 0,
-          "holds_count": 219,
-          "id": "IV4GzXxAvn5WhFBZR9RosL6U",
-          "holds_total_amount": 1378117,
-          "reversals_total_fee": 0,
-          "lost_debit_chargebacks_total_amount": 0,
-          "failed_credit_fee": 0,
-          "state": "paid",
-          "chargeback_fixed_fee": 1500,
-          "reversals_count": 0,
-          "card_debits_total_amount": 741118,
-          "holds_total_fee": 0,
-          "reversals_total_amount": 0,
-          "card_debits_count": 171,
-          "debits_total_fee": 24086,
-          "bank_account_credit_fee": 0,
-          "refunds_total_fee": 0,
-          "bank_account_credits_total_fee": 0,
-          "period": [
-            "2013-11-09T00:01:31.148171Z",
-            "2013-11-09T23:57:33.609046Z"
-          ],
-          "failed_credits_count": 0,
-          "refunds_total_amount": 0,
-          "adjustments_total_fee": 0,
-          "refund_fee": true,
-          "settle_at": "2013-11-10T01:29:54.315160Z",
-          "bank_account_debits_total_fee": 0,
-          "lost_debit_chargebacks_count": 0,
-          "hold_fee": 0,
-          "bank_account_credits_total_amount": 1166031,
-          "debits_total_amount": 741118,
-          "uri": "/invoices/IV4GzXxAvn5WhFBZR9RosL6U",
-          "card_debits_total_fee": 24086,
-          "bank_account_debit_variable_fee_percentage": 1.0,
-          "href": "/invoices/IV4GzXxAvn5WhFBZR9RosL6U",
-          "adjustments_count": 0,
-          "total_fee": 24086,
-          "variable_fee_percentage": 3.25,
-          "failed_credits_total_amount": 0,
-          "debits_count": 171,
-          "lost_debit_chargebacks_total_fee": 0,
-          "bank_account_debits_count": 0,
-          "sequence_number": 325
-        }
-      ],
-      "links": {
-        "invoices.source": "/resources/{invoices.source}",
-        "invoices.settlement": "/invoice_settlements/{invoices.invoice_settlement}"
-      }
-    }
-  },
-  "marketplace_guid": "MP35BtXWqGuYEsv2RHH2CGtH",
-  "occurred_at": "2013-11-10T02:01:08.980000Z",
-  "entity_guid": "IV4GzXxAvn5WhFBZR9RosL6U",
-  "callbacks": [],
-  "internal": false,
-  "entity_data": {
-    "bank_account_credits_count": 12,
-    "account_debit_variable_fee_percentage": 1.0,
-    "cheque_credits_total_amount": 0,
-    "account_debits_total_fee": 0,
-    "cheque_credits_count": 0,
-    "cheque_credits_total_fee": 0,
-    "updated_at": "2013-11-10T02:01:08.980658Z",
-    "failed_credits_total_fee": 0,
-    "account_debits_total_amount": 0,
-    "variable_fee_percentage": 3.25,
-    "refunds_count": 0,
-    "bank_account_debits_total_amount": 0,
-    "holds_count": 219,
-    "guid": "IV4GzXxAvn5WhFBZR9RosL6U",
-    "holds_total_amount": 1378117,
-    "cheque_credit_fixed_fee": 100,
-    "account_debit_variable_fee_cap": null,
-    "reversals_total_fee": 0,
-    "marketplace_guid": "MP35BtXWqGuYEsv2RHH2CGtH",
-    "lost_debit_chargebacks_total_amount": 0,
-    "refund_fee": true,
-    "prepaid_discount_percentage": 2.0,
-    "reversal_fee": 0,
-    "settle_at": "2013-11-10T01:29:54.315160Z",
-    "card_debits_total_amount": 741118,
-    "holds_total_fee": 0,
-    "reversals_total_amount": 0,
-    "card_debits_count": 171,
-    "bank_account_credit_fee": 0,
-    "min_created_at": "2013-11-09T00:01:31.148171Z",
-    "refunds_total_fee": 0,
-    "settle_lock": 1,
-    "bank_account_credits_total_fee": 0,
-    "failed_credits_total_amount": 0,
-    "failed_credits_count": 0,
-    "refunds_total_amount": 0,
-    "bank_account_debit_fee": 30,
-    "adjustments_total_fee": 0,
-    "failed_credit_fee": 0,
-    "reversals_count": 0,
-    "bank_account_debits_total_fee": 0,
-    "count": 402,
-    "lost_debit_chargebacks_count": 0,
-    "hold_fee": 0,
-    "chargeback_liability_percentage": 100.0,
-    "bank_account_credits_total_amount": 1166031,
-    "account_debits_count": 0,
-    "created_at": "2013-11-10T01:29:54.367778Z",
-    "account_debit_fixed_fee": 0,
-    "card_debits_total_fee": 24086,
-    "bank_account_debit_variable_fee_percentage": 1.0,
-    "bank_account_debit_variable_fee_cap": 500,
-    "chargeback_fixed_fee": 1500,
-    "adjustments_count": 0,
-    "total_fee": 24086,
-    "max_created_at": "2013-11-09T23:57:33.609046Z",
-    "lost_debit_chargebacks_total_fee": 0,
-    "funding_instrument_guid": "BA3EcoA5u8GOmm1EipSUDlPM",
-    "bank_account_debits_count": 0,
-    "sequence_number": 325
-  },
-  "guid": "EVbe5fb1a15d2d11e38e7428cfe9603dcf",
-  "type": "invoice.updated"
-}
-    '''
+    import os
     import json
     processor = EventProcessor()
-    event_json = json.loads(paylod)
-    processor.process(event_json)
 
+    logger = logging.getLogger(__name__)
+
+    input_dir = 'input'
+    for filename in os.listdir(input_dir):
+        if filename.startswith('.'):
+            continue
+        filepath = os.path.join(input_dir, filename)
+        logger.info('Loading %s', filepath)
+        with open(filepath, 'rt') as json_file:
+            content = json_file.read()
+            if not content.strip():
+                logger.warn('Ignore empty file %s', filepath)
+                continue
+            event_json = json.loads(content)
+        processor.process(event_json)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
